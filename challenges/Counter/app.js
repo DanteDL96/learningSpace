6826f2098bde7d6449455d18dd56d854b27aa0d9
Daniel <39098553+Dantedl@users.noreply.github.com>
2024-03-16 22:10:29 -0400

    more files to have

---
+++ challenges/Counter/app.js
@@ -0,0 +1,39 @@
+// set inisitial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function(e){
+//         const styles = e.currentTarget.classList;
+//         if(styles.contains('decrease')){
+//             count--;
+//         }
+//         value.textContent = count;
+//     })
+// });
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+            styles
+        }else if(styles.contains('increase')){
+            count ++;
+        } else{
+            count = 0;
+        };
+
+        if(count>0){
+            value.style.color = "green";
+        }else if(count<0){
+            value.style.color = "red";
+        }else {
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    })
+});
